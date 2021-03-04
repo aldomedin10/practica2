9a8281059e0714d2b362313c8edc720880c260b1
aldo medina <aldomedina_10@hotmail.com>
2021-03-03 18:33:58 -0800

    caulculo perimetro

---
+++ calculoperimetro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoperimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir variables
+            int a = 10;
+            int b = 10;
+            int c = 7;
+            int p = a + b + c;
+            Console.WriteLine("el perimetro del trinagulo es:" + p);
+            Console.WriteLine("presione cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
