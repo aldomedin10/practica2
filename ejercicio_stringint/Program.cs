fc2a4005075a95b40b74a16f1f77b3adaa4a519d
aldo medina <aldomedina_10@hotmail.com>
2021-03-04 18:27:57 -0800

    programa que escribe edad nombre  y se hizo una conversion

---
+++ ejercicio_stringint/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_stringint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string lineaedad;
+            int edad;
+            Console.WriteLine("escribe tu nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("escribe la edad:");
+            lineaedad = Console.ReadLine();
+            edad = Convert.ToInt32(lineaedad);
+            Console.WriteLine("tu nombre es:" + nombre + " y tienes " + edad + " anos ");
+            Console.WriteLine("presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
